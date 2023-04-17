566d57dea51b32638a4f02d00a1fbe8f528be714
EvianTourLife <zamaltdinovdmitrij@gmail.com>
2023-04-17 15:48:31 +0400

    added @AuthenticationPrincipal to user controller

--- src/main/java/com/example/bssecurity_3_1_4/controllers/UserController.java
+++ src/main/java/com/example/bssecurity_3_1_4/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.example.bssecurity_3_1_4.controllers;
 import com.example.bssecurity_3_1_4.model.User;
 import com.example.bssecurity_3_1_4.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +20,10 @@ public class UserController {
     public UserController(UserServiceImpl service) {
         this.service = service;
     }
-    @GetMapping()
-    public String getUserInfo(Model model, Principal principal) {
-        User user = (User) service.loadUserByUsername(principal.getName());
-        model.addAttribute("user", service.getById(user.getId()));
 
+    @GetMapping()
+    public String getUserInfo(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("user", user);
         return "userInfo";
     }
 }
