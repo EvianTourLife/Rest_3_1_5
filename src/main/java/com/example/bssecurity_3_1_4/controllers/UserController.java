dbb77b4cdc90650d726ca9e2168e6d0117de1508
EvianTourLife <zamaltdinovdmitrij@gmail.com>
2023-04-17 15:49:11 +0400

    added @AuthenticationPrincipal to user controller(refactoring)

--- src/main/java/com/example/bssecurity_3_1_4/controllers/UserController.java
+++ src/main/java/com/example/bssecurity_3_1_4/controllers/UserController.java
@@ -1,25 +1,17 @@
 package com.example.bssecurity_3_1_4.controllers;
 
 import com.example.bssecurity_3_1_4.model.User;
-import com.example.bssecurity_3_1_4.service.UserServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    private final UserServiceImpl service;
 
-    @Autowired
-    public UserController(UserServiceImpl service) {
-        this.service = service;
-    }
 
     @GetMapping()
     public String getUserInfo(Model model, @AuthenticationPrincipal User user) {
